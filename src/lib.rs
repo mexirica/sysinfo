4ba0e61d5453327b1f179b1d4c2bf04fc337b814
Rodrigo Mecheri <rodrigobme@hotmail.com>
2024-01-10 10:06:31 +0100

    help Commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,69 @@
-mod sysfn;
+use sysinfo::{Disks, Networks, System};
+
+pub fn cpu(sys: &mut System) {
+    sys.refresh_cpu();
+    let mut i: u8 = 1;
+    for cpu in sys.cpus() {
+        println!("number: {}, usage: {:?}", i, cpu.cpu_usage());
+        i += 1;
+    }
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+}
+
+#[no_mangle]
+pub fn ram(sys: &mut System) {
+    sys.refresh_memory();
+    println!("total: {:?} GB, usage: {:?} GB",format_bytes(sys.total_memory()), format_bytes(sys.used_memory()))
+}
+
+#[no_mangle]
+pub fn sys_info(num: u8) {
+        println!("OS: {:?}",System::name().unwrap());
+        println!("Kernel: {:?}",System::kernel_version().unwrap());
+        println!("Version: {:?}",System::os_version().unwrap());
+        println!("Host: {:?}",System::host_name().unwrap());
+        println!("Number CPUs: {:?}",{num});
+}
+
+
+#[no_mangle]
+pub fn disk_info(sys: &mut System) {
+    sys.refresh_all();
+    let disks = Disks::new_with_refreshed_list();
+    let mut i =1;
+    for disk in &disks {
+        println!("Disk: {}", &i);
+        println!("Name: {:?}",disk.name());
+        println!("File system: {:?}",disk.file_system());
+        println!("Disk type: {:?}",disk.kind());
+        println!("Removable: {:?}",disk.is_removable());
+        println!("Mount: {:?}",disk.mount_point());
+        println!("Total size: {:?} GB",format_bytes(disk.total_space()));
+        i += 1;
+    }
+}
+#[no_mangle]
+pub fn network_info(){
+    let networks = Networks::new_with_refreshed_list();
+    for (interface_name, data) in &networks {
+        println!("Name: {}",interface_name);
+        println!("Data received: {:?}",format_bytes(data.received()));
+        println!("Data transmitted: {:?}",format_bytes(data.transmitted()))
+    }
+}
+
+fn format_bytes(bytes: u64) -> u64 {
+    const KB: u64 = 1024;
+    const MB: u64 = KB * 1024;
+    const GB: u64 = MB * 1024;
+
+    if bytes >= GB {
+        bytes / GB
+    } else if bytes >= MB {
+        bytes / MB
+    } else if bytes >= KB {
+        bytes / KB
+    } else {
+        bytes
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,46 @@
-use std::sync::{Arc, Mutex, RwLock};
-use std::thread;
-use sysinfo::Signal::Sys;
-use sysinfo::System;
+use sysinfo::{System};
+use std::env;
+mod lib;
+fn main() {
+    let mut sys = System::new_all();
+    let args: Vec<String> = env::args().collect();
 
-mod sysfn;
+    let args_len = args.len();
+    let first_argument: &String;
+    let second_argument: &String;
 
-fn main() {}
+    match args_len {
+        2 => {
+            first_argument = &args[1];
+        },
+        3 => {
+            first_argument = &args[1];
+            second_argument = &args[2];
+        }
+        _ => {
+            eprintln!("Use: {} <argument>\nTo get help use help as flag", &args[0]);
+            std::process::exit(1);
+        },
+    }
+
+    match first_argument.as_str() {
+        "cpu" => lib::cpu(&mut sys),
+        "ram" => lib::ram(&mut sys),
+        "sysinfo" => lib::sys_info(sys.cpus().len() as u8),
+        "disk" => lib::disk_info(&mut sys),
+        "net" => lib::network_info(),
+        "help" => {
+            println!("To get information about your pc use the followed flags:");
+            println!("cpu --> the percent of use of each cpu");
+            println!("ram --> the total RAM and the usage");
+            println!("sysinfo --> general informations about your pc");
+            println!("disk --> general informations about the disk");
+            println!("net --> name, data received and transmitted")
+        }
+        _ => {
+            eprintln!("Use: {} <argument>", args[0]);
+            std::process::exit(1);
+        }
+    }
+
+}
